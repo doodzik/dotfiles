d21b791a4730f432d0562025f627e036cc7d45d8
Frederik Dudzik <frederik.dudzik@gmail.com>
2015-10-07 10:04:54 +0200

    fix that my .zshrc is overwritten by oh-my-zsh

--- .init/.init_basic.sh
+++ .init/.init_basic.sh
@@ -9,13 +9,6 @@ brew install git
 brew install nvm
 brew install selecta
 
-# setup repo for dotfiles
-git init
-git remote add origin https://github.com/doodzik/dotfiles.git
-git fetch
-git checkout -t origin/master
-git submodule update --init --recursive
-
 # setup node
 . $(brew --prefix nvm)/nvm.sh
 nvm use 4
@@ -26,5 +19,14 @@ defaults write com.apple.Terminal AppleFontSmoothing -int 0
 
 # install ohmyzsh
 command -v zsh >/dev/null 2>&1 || sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+rm .zshrc
+
+# setup repo for dotfiles
+git init
+git remote add origin https://github.com/doodzik/dotfiles.git
+git fetch
+git checkout -t origin/master
+git submodule update --init --recursive
 
 open .dudzik.terminal
