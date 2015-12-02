feda373d06f01e9a220fabb50c5227a32c16d1dd
Frederik Dudzik <frederik.dudzik@gmail.com>
2015-12-03 00:02:01 +0100

    change screencapture location

--- .init/.init_gui.sh
+++ .init/.init_gui.sh
@@ -7,11 +7,14 @@ brew cask install appcleaner
 brew cask install anki
 
 defaults write com.apple.finder CreateDesktop -bool FALSE
+defaults write com.apple.screencapture location ~/Downloads
 
 ln -s Library/Mobile\ Documents/com~apple~CloudDocs
 mv com\~apple\~CloudDocs iCloud
 
-# TODO print each service to log into
 echo "log into:\n"
 echo "anki\n"
 echo "skype\n"
+
+killall SystemUIServer
+killall Finder
